913e93d91d58e5cecf7dfba275cbc0fbb62c63bb
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-24 10:15:41 +0300

    Adding: Main content

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+
+const MainHeading = () => <h1 className="main--title">Fun facts about React</h1>
+const MainList = () => <ul className="main--facts">
+    <li>Was first released in 2013</li>
+    <li>Was originally created by Jordan Walke</li>
+    <li>Has well over 100K stars on GitHub</li>
+    <li>Is maintained by Facebook</li>
+    <li>Powers thousands of enterprise apps, including mobile apps</li>
+</ul>
+
+export default function Main() {
+    return (
+        <main>
+            <MainHeading />
+            <MainList />
+        </main>
+    )
+}
